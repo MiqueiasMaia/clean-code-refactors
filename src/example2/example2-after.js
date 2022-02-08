fdaaccf1381c8a52592a03607aaf4b1460a3561f
Miqueias Maia <miqueiasmaia7@gmail.com>
2022-02-08 00:50:38 -0300

    refactor: delegate functions to calculate and extract actual digit

--- src/example2/example2-after.js
+++ src/example2/example2-after.js
@@ -1,39 +1,40 @@
+const [FACTOR_DIGIT_1, FACTOR_DIGIT_2] = [10, 11];
+
 exports.validate = function (cpfRaw) {
-    if (!cpfRaw) return;
-    if (cpfRaw.length >= 11 || cpfRaw.length <= 14) {
-        const cpf = cpfCleaner(cpfRaw);
-        if (!cpf.split("").every(c => c === str[0])) {
-            try {
-                let d1, d2;
-                let dg1, dg2, rest;
-                let digito;
-                let nDigResult;
-                d1 = d2 = 0;
-                dg1 = dg2 = rest = 0;
-                for (let nCount = 1; nCount < str.length - 1; nCount++) {
-                    digito = parseInt(str.substring(nCount - 1, nCount));
-                    d1 = d1 + (11 - nCount) * digito;
-                    d2 = d2 + (12 - nCount) * digito;
-                };
-                rest = (d1 % 11);
-                dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-                d2 += 2 * dg1;
-                rest = (d2 % 11);
-                if (rest < 2)
-                    dg2 = 0;
-                else
-                    dg2 = 11 - rest;
-                let nDigVerific = str.substring(str.length - 2, str.length);
-                nDigResult = "" + dg1 + "" + dg2;
-                return nDigVerific == nDigResult;
-            } catch (e) {
-                console.error("Erro !" + e);
-                return false;
-            }
-        } else return false
-    } else return false;
+    if (!cpfRaw) return false;
+    const cpf = cpfCleaner(cpfRaw);
+    if (!cpfSizeValidator(cpf)) return false;
+    if (!cpfEqualsDigitsValidator(cpf)) return false;
+    const digit1 = calculateDigit(cpf, FACTOR_DIGIT_1);
+    const digit2 = calculateDigit(cpf, FACTOR_DIGIT_2);
+    const actualDigit = extractActualDigit(cpf);
+    const calculatedDigit = `${digit1}${digit2}`;
+    return actualDigit === calculatedDigit;
 }
 
 const cpfCleaner = (rawCpf) => {
     return rawCpf.replace(/[\.\-]*/g, "");
+};
+
+const cpfSizeValidator = (cpf) => {
+    return cpf.length === 11;
+};
+
+const cpfEqualsDigitsValidator = (cpf) => {
+    const [firstDigit] = cpf;
+    return [...cpf].every(digit => digit === firstDigit);
+};
+
+const calculateDigit = (cpf, factor) => {
+    let total = 0;
+    for (const digit of cpf) {
+        if (factor > 1)
+            total += parseInt(digit) * factor--;
+    }
+    const rest = total % 11;
+    return rest < 2 ? 0 : 11 - rest;
+};
+
+const extractActualDigit = (cpf) => {
+    return cpf.slice(9);
 }
