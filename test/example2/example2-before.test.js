ba0db4c2f2b58994e733ac9144e40857941c3d10
Miqueias Maia <miqueiasmaia7@gmail.com>
2022-02-07 23:45:43 -0300

    feat: add aditional test cases

--- test/example2/example2-before.test.js
+++ test/example2/example2-before.test.js
@@ -4,6 +4,7 @@ const VALID_CPF = '987.654.321-00';
 const INVALID_CPF = '987.654.321-01';
 const SEQUENTIAL_CPF = '123.456.789-00';
 const IDENTIC_DIGITS_CPF = '111.111.111-11';
+const INCOMPLETE_CPF = '123.456.789';
 
 test("Should validate CPF", () => {
     const isValid = cpfValidator.validate(VALID_CPF);
@@ -22,5 +23,20 @@ test("Shouldn't validate sequential CPF", () => {
 
 test("Shouldn't validate CPF with identic digits", () => {
     const isValid = cpfValidator.validate(IDENTIC_DIGITS_CPF);
+    expect(isValid).toBeFalsy();
+});
+
+test("Shouldn't validate null CPF", () => {
+    const isValid = cpfValidator.validate();
+    expect(isValid).toBeFalsy();
+});
+
+test("Shouldn't validate empty CPF", () => {
+    const isValid = cpfValidator.validate('');
+    expect(isValid).toBeFalsy();
+});
+
+test("Shouldn't validate inconplete CPF", () => {
+    const isValid = cpfValidator.validate(INCOMPLETE_CPF);
     expect(isValid).toBeFalsy();
 });
