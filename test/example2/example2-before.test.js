e3c053dac843ef5a6de50e29ef1bac95650d14cc
Miqueias Maia <miqueiasmaia7@gmail.com>
2022-02-07 23:42:31 -0300

    feat: add test to example 2 before version

---
+++ test/example2/example2-before.test.js
@@ -0,0 +1,26 @@
+const cpfValidator = require('../../src/example2/example2-before');
+
+const VALID_CPF = '987.654.321-00';
+const INVALID_CPF = '987.654.321-01';
+const SEQUENTIAL_CPF = '123.456.789-00';
+const IDENTIC_DIGITS_CPF = '111.111.111-11';
+
+test("Should validate CPF", () => {
+    const isValid = cpfValidator.validate(VALID_CPF);
+    expect(isValid).toBeTruthy();
+});
+
+test("Shouldn't validate invalid CPF", () => {
+    const isValid = cpfValidator.validate(INVALID_CPF);
+    expect(isValid).toBeFalsy();
+});
+
+test("Shouldn't validate sequential CPF", () => {
+    const isValid = cpfValidator.validate(SEQUENTIAL_CPF);
+    expect(isValid).toBeFalsy();
+});
+
+test("Shouldn't validate CPF with identic digits", () => {
+    const isValid = cpfValidator.validate(IDENTIC_DIGITS_CPF);
+    expect(isValid).toBeFalsy();
+});
